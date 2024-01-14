6dc6490dc9e99645453e9f2daab04b78c4c4ae95
Aamir Khan <aamir.campus@gmail.com>
2024-01-14 21:28:07 +0530

    snakes and ladders first commit

---
+++ src/main/java/com/example/snakesandladders/App.java
@@ -0,0 +1,7 @@
+package com.example.snakesandladders;
+
+public class App {
+    public static void main(String[] args) {
+        SnakeAndLadders.main(args);
+    }
+}
---
+++ src/main/java/com/example/snakesandladders/Dice.java
@@ -0,0 +1,9 @@
+package com.example.snakesandladders;
+
+public class Dice
+{
+    public int getRolledDiceValue()
+    {
+        return (int)(Math.random()*6+1);
+    }
+}
